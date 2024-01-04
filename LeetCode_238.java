9f56c878f6f66db0e0043d4d3a2392e5d0b29fa9
AnshSaxena05 <114876476+AnshSaxena05@users.noreply.github.com>
2024-01-04 12:57:52 +0530

    Add files via upload

---
+++ LeetCode_238.java
@@ -0,0 +1,33 @@
+public class LeetCode_238
+{
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int result[] = new int[n];
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        int suffixProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= suffixProduct;
+            suffixProduct *= nums[i];
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        LeetCode_238 solution = new LeetCode_238();
+
+        // Example usage with a sample input array
+        int[] nums = {1, 2, 3, 4};
+        int[] result = solution.productExceptSelf(nums);
+
+        // Print the result
+        System.out.print("Result: [");
+        for (int i = 0; i < result.length; i++)
+        {
+            System.out.print(result[i]+", ");
+        }
+        System.out.println("]");
+    }
+}
